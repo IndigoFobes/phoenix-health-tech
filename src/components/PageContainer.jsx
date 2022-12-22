5286a71313b9027309b9d8f0a60a1773c2af0946
Indigo <indigo.crandell@gmail.com>
2022-12-22 11:23:55 -0500

    Added a Header component and set up PageContainer to deal with page rendering and navigation tabs

---
+++ src/components/PageContainer.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Header from "./Header";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Home from "../pages/Home";
+import Landing from "../pages/Landing";
+import Learn from "../pages/Learn";
+import Purchase from "../pages/Purchase";
+
+export default function PageContainer() {
+  // useState to set which page user is on
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    } else if (currentPage === "About") {
+      return <About />;
+    } else if (currentPage === "Contact") {
+      return <Contact />;
+    } else if (currentPage === "Landing") {
+      return <Landing />;
+    } else if (currentPage === "Learn") {
+      return <Learn />;
+    }
+    return <Purchase />;
+  };
+
+  //handle page change; whichever page is plugged in here will be rendered
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  // return
+  return (
+    <div>
+      <main>
+        <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()}
+      </main>
+      <Footer />
+    </div>
+  );
+}
