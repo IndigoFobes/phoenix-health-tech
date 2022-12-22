c631bc77ad19f4865f0a0ab583ced7d696457966
Indigo <indigo.crandell@gmail.com>
2022-12-22 14:12:11 -0500

    Got image to resize oh my word. Had to use fractions.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 
 export default function Header({ currentPage, handlePageChange }) {
   return (
     <header>
-      <h1>Insert PHT logo and title here!</h1>
+      <Link to="/">
+        <h1>Insert PHT logo and title here!</h1>
+      </Link>
       <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
     </header>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PHTLogo from "../assets/phoenix_title.png";
 
 export default function Landing() {
   return (
-    <div className="flex justify-center pt-20">
-      <div className="">
+    <div className="flex justify-center">
+      <div className="flex flex-col">
+        <div className="px-4 mx-auto flex justify-center">
+          <img
+            className="w-3/4 align-middle object-cover object-top mb-20"
+            src={PHTLogo}
+            alt="PHT Logo, colorful phoenix"
+          ></img>
+        </div>
         <h1>LOGO HERE</h1>
         <p>Tag line here</p>
       </div>
