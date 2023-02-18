1ef5f5e212ba988b357661c373333adf8d1b6a7a
Indigo <indigo.crandell@gmail.com>
2023-02-18 14:10:49 -0500

    Duplicated layout from Home in About and Purchase.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,10 +3,36 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <container className="text-center p-10">
-        <h1>PHT About Page</h1>
-        <p>About text</p>
-      </container>
+      <div className="grid grid-cols-6">
+        <container className="col-start-2 col-end-5 mb-5 mt-10">
+          <h1 className="font-sans font-semibold text-2xl text-blue-900 ">
+            About Phoenix Health Technology
+          </h1>
+        </container>
+      </div>
+      <div className="grid grid-cols-6">
+        <container className="col-start-2 col-end-6">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            laoreet a nisl sit amet facilisis. Vestibulum id tellus felis.
+            Vestibulum efficitur dignissim pulvinar. Sed vulputate posuere
+            ultrices. Integer blandit dui ut laoreet bibendum. Maecenas
+            tristique, diam a mollis euismod, nunc erat ultricies orci, vel
+            auctor ex nulla in erat. Ut imperdiet, purus a porttitor dignissim,
+            nisi neque ultrices eros, a commodo lectus lectus egestas leo. Ut
+            quis neque elit. In congue elit rhoncus justo viverra congue. Fusce
+            ultricies nisi a lacus gravida, in efficitur est finibus. Aliquam
+            varius massa vel mi ornare malesuada.
+          </p>
+        </container>
+      </div>
+      <div className="grid grid-cols-6">
+        <container className="col-start-2 col-end-5 mb-5 mt-10">
+          <h1 className="font-sans font-semibold text-2xl text-blue-900 ">
+            Meet Our Team
+          </h1>
+        </container>
+      </div>
     </div>
   );
 }
