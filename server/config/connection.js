1311450d1837cea5b57aeb4b245d09f517159085
Indigo <indigo.crandell@gmail.com>
2023-02-17 21:06:08 -0500

    I didn't have flowbit properly linked to the html. But now it's still a little weird. Lost some of my styling, but we're much better than before.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+// const mongoose = require("mongoose");
+// require("dotenv").config();
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/phoenixDB", // what should this be??
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/phoenixDB", // what should this be?? How do I create a connection?
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
