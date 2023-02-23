e24a28dd7ed8d729ec022472d291c7c51f104f77
Indigo <indigo.crandell@gmail.com>
2023-02-23 11:10:47 -0500

    I would LOVE to have the pop up modal logic housed in a separate jsx file, but right now I can onyl get it working within home. But mouseEnter is working for yellow box.

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+// import React, { useState } from "react";
 
-export default function Modal() {
-  // probably will need to pass in props (id of hovered icon), but idk yet.
-  const [hover, setHover] = React.useState(false);
-  const onHover = () => {
-    setHover(true);
-  };
+// export default function Modal() {
+//   // probably will need to pass in props (id of hovered icon), but idk yet.
+//   // use State
+//   const [isHovering, setisHovering] = useState(false);
 
-  const onLeave = () => {
-    setHover(false);
-  };
-}
+//   // handle mouse enter
+//   function handleMouseEnter(e) {
+//     console.log(e.target);
+//     setisHovering(true);
+//   }
+
+//   return (
+//     <div
+//       className={`bg-amber-300 justify-center mt-10 mx-60 rounded-md py-10 px-20 flex-col text-center gap-5 ${
+//         isHovering ? "flex" : "hidden"
+//       }`}
+//     >
+//       <h2 className="font-semibold text-2xl">An All Natural Solution</h2>
+//       <p className="font-medium">
+//         Mauris in sagittis metus. Sed sodales odio id sem sodales, sed bibendum
+//         risus ultrices. Maecenas magna velit, imperdiet vel dui id, eleifend
+//         fermentum odio. Integer at ultricies mi, nec eleifend dolor. Aliquam
+//         fermentum nunc at libero tempus, sed maximus erat placerat. Praesent in
+//         arcu vitae neque cursus pulvinar.Lorem ipsum dolor sit amet, consectetur
+//         adipiscing elit. Vestibulum laoreet a nisl sit amet facilisis.
+//         Vestibulum id tellus felis. Vestibulum efficitur dignissim pulvinar. Sed
+//         vulputate posuere ultrices. Integer blandit dui ut laoreet bibendum.
+//         Maecenas tristique, diam a mollis euismod, nunc erat ultricies orci, vel
+//         auctor ex nulla in erat. Ut imperdiet, purus a porttitor dignissim, nisi
+//         neque ultrices eros, a commodo lectus lectus egestas leo. Ut quis neque
+//         elit.
+//       </p>
+//     </div>
+//   );
+// }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import Modal from "../components/Modal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,8 +7,17 @@ import {
   brands,
   icon,
 } from "@fortawesome/fontawesome-svg-core/import.macro";
+// import Modal from "../components/Modal";
 
 export default function Home() {
+  const [isHovering, setisHovering] = useState(false);
+
+  // handle mouse enter
+  function handleMouseEnter(e) {
+    console.log(e.target);
+    setisHovering(true);
+  }
+
   return (
     <div>
       <div className="grid grid-cols-6">
@@ -58,7 +67,7 @@ export default function Home() {
       </div>
       {/* placeholder icons */}
       <container className="flex justify-around mt-20 mx-60">
-        <div className="flex flex-col">
+        <div className="flex flex-col" onMouseEnter={handleMouseEnter}>
           <FontAwesomeIcon
             className="text-amber-400 text-5xl mt-1.5"
             icon={icon({
@@ -89,9 +98,27 @@ export default function Home() {
           <h3 className="font-semibold text-xl">Prevention First</h3>
         </div>
       </container>
-      {/* ***TODO: This is where I'll pass through the Modal, which will be determined via useState */}
-      {/* {hover ? (
-      <> </> ): null} */}
+      <div
+        className={`bg-amber-300 justify-center mt-10 mx-60 rounded-md py-10 px-20 flex-col text-center gap-5 ${
+          isHovering ? "flex" : "hidden"
+        }`}
+      >
+        <h2 className="font-semibold text-2xl">An All Natural Solution</h2>
+        <p className="font-medium">
+          Mauris in sagittis metus. Sed sodales odio id sem sodales, sed
+          bibendum risus ultrices. Maecenas magna velit, imperdiet vel dui id,
+          eleifend fermentum odio. Integer at ultricies mi, nec eleifend dolor.
+          Aliquam fermentum nunc at libero tempus, sed maximus erat placerat.
+          Praesent in arcu vitae neque cursus pulvinar.Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Vestibulum laoreet a nisl sit amet
+          facilisis. Vestibulum id tellus felis. Vestibulum efficitur dignissim
+          pulvinar. Sed vulputate posuere ultrices. Integer blandit dui ut
+          laoreet bibendum. Maecenas tristique, diam a mollis euismod, nunc erat
+          ultricies orci, vel auctor ex nulla in erat. Ut imperdiet, purus a
+          porttitor dignissim, nisi neque ultrices eros, a commodo lectus lectus
+          egestas leo. Ut quis neque elit.
+        </p>
+      </div>
     </div>
   );
 }
