0a4f9c7a7a69042bb37ac375a259b25c0811d7ad
Indigo <indigo.crandell@gmail.com>
2023-03-24 13:54:27 -0400

    Background image on About page.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,35 +1,35 @@
 import React from "react";
 import indigo from "../assets/indigo_recent.JPG";
+import missionShape from "../assets/pht_mission_shape.png";
+import missionShapeLight from "../assets/pht_mission_shape_light.png";
+import office from "../assets/pexels-office.jpg";
 
 export default function About() {
   return (
-    <div>
-      <div className="grid grid-cols-6">
-        <container className="col-start-1 col-end-7 lg:col-start-2 lg:col-end-6 mx-8 mb-5 mt-10 md:mx-20 ">
-          <h1 className="font-sans text-center md:text-left font-semibold sm:text-xl md:text-2xl lg:text-3xl title-text ">
-            About Phoenix Health Technology
-          </h1>
-        </container>
+    <div className="bg-fixed about-bg relative">
+      <div className="shadow-inner pb-8 bg-gray-200 opacity-80 w-full h-[270px] sm:h-[300px] md:h-[320px] lg:h-[270px]"></div>
+      <div className="absolute top-0 left-0">
+        <div className="grid grid-cols-6">
+          <container className="col-start-1 col-end-7 md:col-end-5 mb-5 mt-10 mx-10">
+            <h1 className="text-center text-shadow md:text-left our-mission title-text drop-shadow-md">
+              Our Mission
+            </h1>
+          </container>
+        </div>
+        <div className="grid grid-cols-6">
+          <container className="col-start-1 col-end-7 md:col-end-4 mb-5 mx-10 text-justify dm-sans-med md:text-left md:text-lg">
+            <p>
+              We exist to create a safe and healthy environment for homes and
+              workspaces using green, state of the art technology based on all
+              natural and organic ingredients.
+            </p>
+          </container>
+        </div>
       </div>
-      <div className="grid grid-cols-6">
-        <container className="col-start-1 col-end-7 mx-8 md:mx-20 lg:col-start-2 lg:col-end-6 text-justify md:text-left text-sm sm:text-base">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            laoreet a nisl sit amet facilisis. Vestibulum id tellus felis.
-            Vestibulum efficitur dignissim pulvinar. Sed vulputate posuere
-            ultrices. Integer blandit dui ut laoreet bibendum. Maecenas
-            tristique, diam a mollis euismod, nunc erat ultricies orci, vel
-            auctor ex nulla in erat. Ut imperdiet, purus a porttitor dignissim,
-            nisi neque ultrices eros, a commodo lectus lectus egestas leo. Ut
-            quis neque elit. In congue elit rhoncus justo viverra congue. Fusce
-            ultricies nisi a lacus gravida, in efficitur est finibus. Aliquam
-            varius massa vel mi ornare malesuada.
-          </p>
-        </container>
-      </div>
-      <div className="mb-10 md:mb-20 grid grid-cols-6">
+      <div className="w-full h-[500px]"></div>
+      <div className="mb-10 md:mb-20 grid grid-cols-6 bg-white">
         <div className="col-start-2 col-end-6 mb-5 mt-10">
-          <h1 className="font-sans text-center font-semibold text-2xl sm:text-xl md:text-2xl title-text sm:ml-10">
+          <h1 className="font-sans text-center font-semibold text-xl md:text-2xl title-text sm:ml-10">
             Meet Our Team
           </h1>
         </div>
