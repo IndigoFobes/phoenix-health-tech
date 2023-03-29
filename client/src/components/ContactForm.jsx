1ac401a0528042251e003b26f2cdd71dcf85caac
Indigo <indigo.crandell@gmail.com>
2023-03-29 16:47:16 -0400

    Working through contact changes. More elements spanning screen width.

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -33,48 +33,58 @@ const ContactForm = () => {
   };
 
   return (
-    <div className="h-screen flex items-center mt-40">
-      <container className="bg-gradient-to-br from-sky-600  to-sky-300 grid grid-cols-5 mx-auto max-w-screen-lg p-4 rounded-md mb-40">
-        {/* Left side: "Contact us" with Social media icons and such */}
-        <div className="col-start-1 col-end-3 pl-5 pr-20">
-          <h1 className="text-3xl font-bold dm-sans-bold text-white mt-3 mb-5">
-            Contact us
-          </h1>
-          <p className="dm-sans text-xl text-white mb-9">
-            Send us a message, and we will be sure to get back to you as soon as
-            possible.
-          </p>
-          <div className="flex flex-row gap-2">
-            <FontAwesomeIcon
-              className="text-white mt-1.5"
-              icon={icon({
-                name: "phone",
-                style: "solid",
-              })}
-            />
-            <p className="text-xl dm-sans text-white mb-3">777-777-7777</p>
-          </div>
-          <div className="flex flex-row gap-2">
-            <FontAwesomeIcon
-              className="text-white mt-1.5"
-              icon={icon({
-                name: "envelope",
-                style: "solid",
-              })}
-            />
-            <p className="text-xl dm-sans text-white mb-3">
-              will@phoenixhealthtech.com
+    <div className="bg-fixed contact-photo flex flex-col">
+      <div className="bg-gray-200/80 py-10">
+        <div className="grid grid-cols-6">
+          <container className="col-start-1 col-end-7 lg:col-end-5 mb-5 mx-10">
+            <h1 className="text-center lg:text-left our-mission title-text">
+              Contact us
+            </h1>
+          </container>
+        </div>
+        <div className="grid grid-cols-6">
+          <container className="col-start-1 col-end-7 lg:col-end-5 xl:col-end-4 mb-5 mx-10 text-center dm-sans-med lg:text-left sm:text-lg md:text-xl">
+            <p>
+              Send us a message, and we will be sure to get back to you as soon
+              as possible.
             </p>
-          </div>
-          {/* TODO: social media icons: NOT working yet}
+          </container>
+        </div>
+      </div>
+      <div className="flex justify-between mx-20 my-40 pl-5 pr-20">
+        <div className="flex flex-row gap-2 contact-icons dm-sans-bold">
+          <FontAwesomeIcon
+            className="text-white mt-1.5"
+            icon={icon({
+              name: "phone",
+              style: "solid",
+            })}
+          />
+          <p className=" text-white mb-3">777-777-7777</p>
+        </div>
+        <div className="flex flex-row gap-2 contact-icons dm-sans-bold">
+          <FontAwesomeIcon
+            className="text-white mt-1.5"
+            icon={icon({
+              name: "envelope",
+              style: "solid",
+            })}
+          />
+          <p className="text-white mb-3">will@phoenixhealthtech.com</p>
+        </div>
+        {/* TODO: social media icons: NOT working yet}
           {/* <div>
             <FontAwesomeIcon className="mt-1.5" icon={brands("twitter")} />
           </div> */}
-        </div>
+      </div>
+
+      <container className="bg-gradient-to-br from-sky-600 to-sky-300 grid grid-cols-5 p-4">
+        {/* Left side: "Contact us" with Social media icons and such */}
+
         {/* input form */}
         <form
           onSubmit={handleSubmit}
-          className="bg-white col-start-3 col-end-6 rounded-r-md p-8 grid grid-rows-2"
+          className="bg-white col-start-3 col-end-6 rounded-r-md p-8 grid grid-rows-2 ml-60 "
         >
           <div className="grid grid-cols-2 gap-3">
             {/* first name */}
